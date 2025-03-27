e22cf7ca0d052a44b9347d9e0c95958160900b04
lce2005 <leece050628@gmail.com>
2025-03-27 10:04:05 +0900

    fix : 2주차 실습 과제제

--- 이채은/chaeeun.py
+++ 이채은/chaeeun.py
@@ -1,1 +1,27 @@
 print("Hello World!")
+
+A, B = map(int,input('0<A,B<10인 두 정수를 입력하시오').split())
+print(A+B)
+
+A, B = map(int,input('0<A,B<10인 두 정수를 입력하시오').split())
+print(A-B)
+
+A, B = map(int,input('0<A,B<10인 두 정수를 입력하시오').split())
+print(A*B)
+
+A, B = map(float,input('0<A,B<10인 두 정수를 입력하시오').split())
+print(A/B)
+
+A, B = map(int,input('1<=A, B<=10,000인 두 정수를 입력하시오').split())
+print(A+B)
+print(A-B)
+print(A*B)
+print(A/B)
+print(A%B)
+
+a=('joonas', 'baekjoon')
+b=str(input())
+if b in a:
+    print(b,'??!')
+else:
+    pass
