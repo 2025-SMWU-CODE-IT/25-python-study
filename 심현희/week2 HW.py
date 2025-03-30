9543ac08c312fd15bb6eb2699f35830c9371ac63
enuyhs39 <shhsystem061221@gmail.com>
2025-03-30 16:16:49 +0900

    feat.[심현희]2025년 1학기 python study - 2주차

---
+++ 심현희/week2 HW.py
@@ -0,0 +1,63 @@
+#문제번호 : 2557
+print("Hello World!")
+#문제번호 : 1000
+a,b = map(int,input().split())
+print(a+b)
+#문제번호 : 1001
+a,b = map(int,input().split())
+print(a-b)
+#문제번호 : 10998
+a,b = map(int,input().split())
+print(a*b)
+#문제번호 : 1008
+a,b = map(int,input().split())
+print(a/b)
+#문제번호 : 10869
+a,b = map(int,input().split())
+print(a+b)
+print(a-b)
+print(a*b)
+print(a//b)
+print(a%b)
+#문제번호 : 10926
+a = input('아이디를 입력하세요 : ')
+print(a+'??!')
+#문제번호 : 18108
+a = input('불기연도를 입력하세요 : ')
+a = int(a)
+print(a-543)
+#문제번호 : 10430
+a,b,c = map(int,input().split())
+print((a+b)%c)
+print(((a%c)+(b%c))%c)
+print((a*b)%c)
+print(((a%c)*(b%c))%c)
+#문제번호 : 2588
+a = input()
+a = int(a)
+b = input()
+b = int(b)
+mylist = []
+for i in str(b) : 
+    mylist.append(i)
+mylist = list(map(int,mylist))
+print(a*mylist[2])
+print(a*mylist[1])
+print(a*mylist[0])
+print(a*b)
+#문제번호 : 11382
+a,b,c = map(int,input().split())
+print(a+b+c)
+#문제번호 : 10171
+a = '''\    /\\
+ )  ( ')
+(  /  )
+ \(__)|'''
+print(a)
+#문제번호 : 10172
+b = '''|\_/|
+|q p|   /}
+( 0 )"""\\
+|"^"`    |
+||_/=\\\\__|'''
+print(b)
