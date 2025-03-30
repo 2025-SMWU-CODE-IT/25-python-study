cfa5e763d5b888c2e8e1f20f2f888950904d1ca4
“juday1036” <democratic99@sookmyung.ac.kr>
2025-03-30 22:17:49 +0900

    feat : done #13

--- 김민주/minju.py
+++ 김민주/minju.py
@@ -1,8 +1,5 @@
-#1주차 과제
-#1
-print("hello world!")
-#2
-A=int(input('0<A,B<10인 두 정수를 입력하시오'))
-B=int(input('0<A,B<10인 두 정수를 입력하시오'))
-print(A+B)
-#3
+print("|\\_/|")
+print("|q p|   /}")
+print('( 0 )"""\\')
+print('| "^" `  |')
+print("||_/=\\\\__|")
