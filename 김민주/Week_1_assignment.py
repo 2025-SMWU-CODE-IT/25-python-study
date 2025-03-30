d1353cd8b3acef859de4edbdf37c5fe35f167632
“juday1036” <democratic99@sookmyung.ac.kr>
2025-03-30 21:45:04 +0900

    feat : #9

---
+++ 김민주/Week_1_assignment.py
@@ -0,0 +1,47 @@
+#1주차 과제
+
+#1
+print("hello world!")
+#2
+A=int(input('0<A,B<10 인 두 정수를 입력하시오 : '))
+B=int(input('0<A,B<10 인 두 정수를 입력하시오 : '))
+print(A+B)
+#3
+A=int(input('0<A,B<10 인 두 정수를 입력하시오 : '))
+B=int(input('0<A,B<10 인 두 정수를 입력하시오 : '))
+print(A-B)
+#4
+A=int(input('0<A,B<10 인 두 정수를 입력하시오 : '))
+B=int(input('0<A,B<10 인 두 정수를 입력하시오 : '))
+print(A*B)
+#5
+A=int(input('0<A,B<10 인 두 정수를 입력하시오 : '))
+B=int(input('0<A,B<10 인 두 정수를 입력하시오 : '))
+print(A/B)
+#6
+A=int(input('0<A,B<10,000 인 두 정수를 입력하시오 : '))
+B=int(input('0<A,B<10,000 인 두 정수를 입력하시오 : '))
+print(A+B)
+print(A-B)
+print(A*B)
+print(A/B)
+print(A%B)
+#7
+A = input('50자 이하의 아이디를 입력해주세요 : ')
+print(A+"??!")
+#8
+y=int(input('불기 연도를 입력하세요 : '))
+print(y-543)
+#9
+
+
+
+
+
+
+print("\\    /\\")
+print(" )  ( ')")
+print("(  /  )")
+print(" \(__)|")
+#10
+map(int())
---
+++ 김민주/minju.py
@@ -0,0 +1,8 @@
+#1주차 과제
+#1
+print("hello world!")
+#2
+A=int(input('0<A,B<10인 두 정수를 입력하시오'))
+B=int(input('0<A,B<10인 두 정수를 입력하시오'))
+print(A+B)
+#3
