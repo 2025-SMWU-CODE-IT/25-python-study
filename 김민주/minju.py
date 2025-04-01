8b5ff1066678fd5fb142ad967ee899dff6bd48a2
“juday1036” <democratic99@sookmyung.ac.kr>
2025-04-01 18:46:44 +0900

    feat : 3주차 27866

--- 김민주/minju.py
+++ 김민주/minju.py
@@ -1,3 +1,71 @@
+#1주차 과제
+#[김민주] 2025년 1학기 python study - 2주차
+
+#1 2557
+print("hello world!")
+
+#2 1000
+A=int(input('0<A,B<10 인 두 정수를 입력하시오 : '))
+B=int(input('0<A,B<10 인 두 정수를 입력하시오 : '))
+print(A+B)
+
+#3 1001
+A=int(input('0<A,B<10 인 두 정수를 입력하시오 : '))
+B=int(input('0<A,B<10 인 두 정수를 입력하시오 : '))
+print(A-B)
+
+#4 10998
+A=int(input('0<A,B<10 인 두 정수를 입력하시오 : '))
+B=int(input('0<A,B<10 인 두 정수를 입력하시오 : '))
+print(A*B)
+
+#5 1008
+A=int(input('0<A,B<10 인 두 정수를 입력하시오 : '))
+B=int(input('0<A,B<10 인 두 정수를 입력하시오 : '))
+print(A/B)
+
+#6 10869
+A=int(input('0<A,B<10,000 인 두 정수를 입력하시오 : '))
+B=int(input('0<A,B<10,000 인 두 정수를 입력하시오 : '))
+print(A+B)
+print(A-B)
+print(A*B)
+print(A/B)
+print(A%B)
+
+#7 10926
+A = input('50자 이하의 아이디를 입력해주세요 : ')
+print(A+"??!")
+
+#8 18108
+y=int(input('불기 연도를 입력하세요 : '))
+print(y-543)
+
+#9 10430
+A, B, C = map(float,input('2 <= A, B, C <= 10000인 세 정수를 입력하시오').split())
+print((A+B)%C)
+print(((A%C) + (B%C))%C)
+print((A*B)%C)
+print(((A%C) * (B%C))%C)
+
+#10 2588
+N1, N2 = map(int, input("두개의 세자리 자연수를 차례대로 입력하세요 : ").split())
+print(N1*(N2%10))
+print(N1*(N2%100//10))
+print(N1*(N2//100))
+print(N1*N2)
+
+#11 11382
+A, B, C = map(int,input('1 <= A, B, C <= 10^12인 세 정수를 입력하시오').split())
+print(A+B+C)
+
+#12 10171
+print("\\    /\\")
+print(" )  ( ')")
+print("(  /  )")
+print(" \(__)|")
+
+#13 10172
 print("|\\_/|")
 print("|q p|   /}")
 print('( 0 )"""\\')
