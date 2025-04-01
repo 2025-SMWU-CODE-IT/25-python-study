b3a819e64a358220ff85cd21248f065807fc4b79
lce2005 <leece050628@gmail.com>
2025-04-01 20:20:15 +0900

    feat : 3주차 과제제

--- 이채은/w3_codeit.py
+++ 이채은/w3_codeit.py
@@ -20,3 +20,42 @@ if c > d :
     print(c)
 else :
     print(d)
+
+#1330 두 수 비교하기
+a = int(input())
+b = int(input())
+if a > b:
+    print('>')
+elif a = b:
+    print('=')
+else:
+    print('<')
+
+#9498 시험 성적
+a = int(input())
+if 90 <= a <=100:
+    print('A')
+elif 80 <= a <= 89:
+    print('B')
+elif 70 <= a <= 79:
+    print('C')
+elif 60 <= a <= 69:
+    print('D')
+else:
+    print('F')
+
+#2884 알람 시계
+a = input('시')
+b = input('분')
+if b >= 45:
+    print(a,'시',b-45,'분')
+else:
+    print(a-1'시',b+60-45,'분')
+
+#10988 팰린드롬인지 확인하기
+a = input()
+b = len(a)//2
+
+for i in range(b):
+    if a[i] == a[len(a)-1]:
+        pass
