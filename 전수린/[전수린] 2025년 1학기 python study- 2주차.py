43b24667995259c6cfa675eb4af6cf072df826dc
jeonsoorin <jonsorin0066@naver.com>
2025-04-01 18:12:07 +0900

    feat: 2주차 과제제

---
+++ 전수린/[전수린] 2025년 1학기 python study- 2주차.py
@@ -0,0 +1,80 @@
+#2주차 과제
+
+#문제 번호: 2557
+print("Hello World!")
+
+#문제 번호: 1000
+A=int(input("A= "))
+B=int(input("B= "))
+print(A+B)
+
+#문제 번호:1001
+A=int(input("A= "))
+B=int(input("B= "))
+print(A-B)
+
+#문제 번호: 10998
+A=int(input("A= "))
+B=int(input("B= "))
+print(A*B)
+
+#문제 번호: 1008
+A=int(input("A= "))
+B=int(input("B= "))
+print(A/B)
+
+#문제 번호: 10869
+A=int(input("A= "))
+B=int(input("B= "))
+print(A+B)
+print(A-B)
+print(A**B)
+print(A/B)
+print(A%B)
+
+#문제 번호: 10926
+name=input()
+print(name+"??!")
+
+#문제 번호: 18108
+year=int(input("year= "))
+print(year-537)
+
+#문제 번호: 10430
+A=int(input("A= "))
+B=int(input("B= "))
+C=int(input("C= "))
+print((A+B)%C)
+print((A%C)+(B%C)%C)
+print((A*B)%C)
+print(((A%C)*(B%C))%C)
+
+#문제 번호: 2588
+num1=int(input())
+num2=int(input())
+
+one=num2%10
+ten=(num2//10)%10
+hundred=num2//100
+
+print(num1*one)
+print(num1*ten)
+print(num1**hundred)
+print(num1*num2)
+
+#문제 번호: 11382
+A, B, C=map(int, input().split())
+print(A+B+C)
+
+#문제 번호: 10171
+print('/\\    /\\')
+print(" )  ( ')")
+print("(  /  )")
+print("\(__)|")
+
+#문제 번호: 10172
+print("|\_/|")
+print("|q p|   /}")
+print('( 0 )"""\\')
+print('|"^"`    |')
+print("||_/=\\__|")
