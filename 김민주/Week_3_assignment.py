1f585551b4d5a78530890decbfde4575e7a4bfe9
“juday1036” <democratic99@sookmyung.ac.kr>
2025-04-02 12:17:53 +0900

    feat : 최최종

--- 김민주/Week_3_assignment.py
+++ 김민주/Week_3_assignment.py
@@ -1,4 +1,4 @@
-#2주차 과제
+#3주차 과제
 #[김민주] 2025년 1학기 python study - 3주차
 
 #27866
@@ -48,12 +48,14 @@ else:
 #2884
 H = int(input("시간 : "))
 M = int(input("분 : "))
-while H > 24 or M > 60:
+while H > 24 or M > 60: #처음 받은 시간을 판정하기 위함.
     print("다시 설정 해주세요")
-    continue
-alarm = H*60 + M - 45
+    H = int(input("시간 : "))
+    M = int(input("분 : "))
+    continue #만약, 범위 내의 숫자가 아니면 반복문 다시 실행.
+alarm = H*60 + M - 45 #시간도 분으로 바꾼 후, 45분 빼줌.
 H = alarm // 60
-M = str(alarm - H*60)
+M = str(alarm - H*60) #아래의 프린트 문에서 :를 출력하기 위해 자료형을 문자열로 변환해줌.
 H = str(H)
 
 print(H + ":" + M)
@@ -62,11 +64,11 @@ print(H + ":" + M)
 B = input()
 i = 0
 
-if B[0+i] == B[-1-i]:
+if B[0+i] == B[-1-i]: #맨 앞, 맨 뒤부터 하나씩 비교하며 진행됨.
     while True:
         i+=1
         print(1)
-        if i == i:
+        if i == i: #만약 자릿수가 같아질때, 반복문을 탈출.
             break  
 else:
     print(0)
--- 김민주/minju.py
+++ 김민주/minju.py
@@ -1,13 +1,14 @@
 #2884
 H = int(input("시간 : "))
 M = int(input("분 : "))
-while H > 24 or M > 60:
+while H > 24 or M > 60: 
     print("다시 설정 해주세요")
+    H = int(input("시간 : "))
+    M = int(input("분 : "))
     continue
-alarm = H*60 + M - 45
+alarm = H*60 + M - 45 #시간도 분으로 바꾼 후, 45분 빼줌.
 H = alarm // 60
-M = str(alarm - H*60)
+M = str(alarm - H*60) #아래의 프린트 문에서 :를 출력하기 위해 자료형을 문자열로 변환해줌.
 H = str(H)
 
 print(H + ":" + M)
-
