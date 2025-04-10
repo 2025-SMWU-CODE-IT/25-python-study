b39d3bcde5984a546b32d8906c933115dbfd30ce
“juday1036” <democratic99@sookmyung.ac.kr>
2025-04-10 20:16:40 +0900

    fix : 피드백 반영

--- 김민주/Week_3_assignment.py
+++ 김민주/Week_3_assignment.py
@@ -16,11 +16,15 @@ A = input()
 print(ord(A))
 
 #2908
-sang, geun = input("상근 : ").split()
-if sang >= geun :
-    print(sang[2]+sang[1]+sang[0])
-else :
-    print(geun[2]+geun[1]+geun[0])
+sang, geun = input().split()
+
+s1 = sang[2] + sang[1] + sang[0]
+g1 = geun[2] + geun[1] + geun[0]
+
+if int(s1) > int(g1):
+    print(s1)
+else:
+    print(g1)
 
 #1330
 A, B = input().split()
@@ -33,41 +37,42 @@ else :
 
 #9498
 T = int(input())
-if 100>=T>=90 :
+if T >= 90:
     print("A")
-elif 90>T>=80 :
+elif T >= 80:
     print("B")
-elif 80>T>=70 :
+elif T >= 70:
     print("C")
-elif 70>T>=60 :
+elif T >= 60:
     print("D")
 else:
     print("F")
 
-#2884
-H = int(input("시간 : "))
-M = int(input("분 : "))
-while H > 24 or M > 60: #처음 받은 시간을 판정하기 위함.
-    print("다시 설정 해주세요")
-    H = int(input("시간 : "))
-    M = int(input("분 : "))
-    continue #만약, 범위 내의 숫자가 아니면 반복문 다시 실행.
-alarm = H*60 + M - 45 #시간도 분으로 바꾼 후, 45분 빼줌.
-H = alarm // 60
-M = str(alarm - H*60) #아래의 프린트 문에서 :를 출력하기 위해 자료형을 문자열로 변환해줌.
-H = str(H)
 
-print(H + ":" + M)
+#2884
+H, M = input().split()
+H = int(H)
+M = int(M)
+if M < 45:
+    H = (H - 1) % 24
+    M += 15
+else:
+    M -= 45
+print(H, M)
 
 #10988
 B = input()
+length = len(B)
 i = 0
+pelin = 0
 
-if B[0+i] == B[-1-i]: #맨 앞, 맨 뒤부터 하나씩 비교하며 진행됨.
-    while True:
-        i+=1
-        print(1)
-        if i == i: #만약 자릿수가 같아질때, 반복문을 탈출.
-            break  
+for i in range(length//2): #추가, 문자 길이의 절반만 연산하면 앞뒤에서 비교할때 모두 비교가 됨
+    if B[0+i] == B[-1-i]: #맨 앞, 맨 뒤부터 하나씩 비교하며 진행됨. 
+        pelin = True #추가, for 안에 있으면 어떻게든 반복되니깐, 그냥 참 거짓을 판별하여, 마지막에 프린트.
+    else:
+        pelin = False
+    
+if pelin:
+    print(1)
 else:
     print(0)
--- 김민주/minju.py
+++ 김민주/minju.py
@@ -1,8 +1,9 @@
-B = input()
-i = 0
-
-if B[0+i] == B[-1-i]: #맨 앞, 맨 뒤부터 하나씩 비교하며 진행됨.
-    for i in range:
-        i+=1
+H, M = input().split()
+H = int(H)
+M = int(M)
+if M < 45:
+    H = (H - 1) % 24
+    M += 15
 else:
-    print(0)
+    M -= 45
+print(H, M)
