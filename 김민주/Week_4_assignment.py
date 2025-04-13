66622192b1b1c03e92e61d9a4bd0df2134825a0f
“juday1036” <democratic99@sookmyung.ac.kr>
2025-04-13 21:58:22 +0900

    feat : 4주차 #25304

--- 김민주/Week_4_assignment.py
+++ 김민주/Week_4_assignment.py
@@ -7,3 +7,18 @@ i = 1
 
 for i in range(1, 10):
     print("{0} * {1} = {2}".format(N, i, N*i))
+
+#25304
+X = int(input())
+N = int(input())
+plus = 0
+
+for i in range(N):
+    a, b = input().split()
+    plus = plus + int(a) * int(b) 
+
+if plus == X:
+    print("Yes")
+else:
+    print("No")
+
